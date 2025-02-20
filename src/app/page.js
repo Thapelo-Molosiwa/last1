cc4a3d51aa1da5fb9eebf703fb2def5de5837b95
Isaac Thapelo Molosiwa <123638341+Thapelo-Molosiwa@users.noreply.github.com>
2025-02-20 13:27:31 +0200

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Research from "../components/Research";
 import Technology from "../components/Technology";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
